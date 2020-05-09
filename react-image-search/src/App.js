3f091842f3e673146f27929ef0b93fe797da7afd
Ryan Castner <castner.rr@gmail.com>
2020-05-09 19:11:37 -0400

    react done

--- react-image-search/src/App.js
+++ react-image-search/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import getImages from './api'
 
 function App() {
+  const [loading, setLoading] = React.useState(false)
+  const [searchTerm, setSearchTerm] = React.useState('')
+  const [images, setImages] = React.useState([])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setLoading(true)
+    setImages(await getImages(searchTerm))
+    setLoading(false)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Image Search</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="searchTerm">Search Term</label>
+        <input
+          className="u-full-width"
+          type="text"
+          id="searchTerm"
+          name="searchTerm"
+          onChange={(e) => setSearchTerm(e.currentTarget.value)}
+          value={searchTerm}
+        />
+        <button type="submit">Search</button>
+      </form>
+      {loading && (
+        <img id="loadingImage" src="https://i.imgur.com/LVHmLnb.gif" />
+      )}
+      <section className="images">
+        {images.map((image) => (
+          <img key={image} src={image} />
+        ))}
+      </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
